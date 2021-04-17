48105cc9633cc418ab65eb6d883493a07d66ed32
chaos <invalid_ms_user@live.com>
2021-04-17 16:01:46 +0800

    replace magic num

--- 01 Tetris/main.cpp
+++ 01 Tetris/main.cpp
@@ -1,37 +1,43 @@
 #include <SFML/Graphics.hpp>
 #include <algorithm>
+#include <array>
 #include <ctime>
 #include <iostream>
 
 using namespace sf;
 
-constexpr int RowsNum = 20;
-constexpr int ColumnsNum = 10;
+constexpr int kRowsNum = 20;
+constexpr int kColumnsNum = 10;
+constexpr int kFiguresNum = 7;
 
-int field[RowsNum][ColumnsNum] = {0};
+int field[kRowsNum][kColumnsNum] = {0};
 
+constexpr int kBlockNumOfPiece = 4;
 struct Point {
   int x, y;
-} next_pos[4], tmp_pos[4];
-
-int figures[7][4] = {
-    1, 3, 5, 7,  // I
-    2, 4, 5, 7,  // Z
-    3, 5, 4, 6,  // S
-    3, 5, 4, 7,  // T
-    2, 3, 5, 7,  // L
-    3, 5, 7, 6,  // J
-    2, 3, 4, 5,  // O
 };
+
+using PointsType = std::array<Point, kBlockNumOfPiece>;
+PointsType next_pos, tmp_pos;
+
+constexpr std::array<PointsType, kFiguresNum> figures = {{
+    {{{1, 0}, {1, 1}, {1, 2}, {1, 3}}},  // I
+    {{{0, 1}, {0, 2}, {1, 2}, {1, 3}}},  // Z
+    {{{1, 1}, {1, 2}, {0, 2}, {0, 3}}},  // S
+    {{{1, 1}, {1, 2}, {0, 2}, {1, 3}}},  // T
+    {{{0, 1}, {1, 1}, {1, 2}, {1, 3}}},  // L
+    {{{1, 1}, {1, 2}, {1, 3}, {0, 3}}},  // J
+    {{{0, 1}, {1, 1}, {0, 2}, {1, 2}}}   // O
+}};
 
 bool escapeButtonStatus = false;
 bool pause = false;
 unsigned score = 0;
 
 bool check() {
-  for (int i = 0; i < 4; i++) {
-    if (next_pos[i].x < 0 || next_pos[i].x >= ColumnsNum ||
-        next_pos[i].y >= RowsNum) {
+  for (int i = 0; i < kBlockNumOfPiece; i++) {
+    if (next_pos[i].x < 0 || next_pos[i].x >= kColumnsNum ||
+        next_pos[i].y >= kRowsNum) {
       return false;
     }
     if (field[next_pos[i].y][next_pos[i].x]) {
@@ -61,10 +67,10 @@ int main() {
   Clock clock;
 
   {
-    const int firstFigure = rand() % 7;
-    for (int i = 0; i < 4; i++) {
-      next_pos[i].x = figures[firstFigure][i] % 2;
-      next_pos[i].y = figures[firstFigure][i] / 2;
+    const int firstFigure = rand() % kFiguresNum;
+    for (int i = 0; i < kBlockNumOfPiece; i++) {
+      next_pos[i].x = figures[firstFigure][i].x;
+      next_pos[i].y = figures[firstFigure][i].y;
     }
   }
 
@@ -74,9 +80,7 @@ int main() {
     Event e;
     while (window.pollEvent(e)) {
       if (e.type == Event::Closed) window.close();
-
       if (e.type == Event::LostFocus) pause = true;
-
       if (e.type == Event::GainedFocus) pause = false;
 
       if (e.type == Event::KeyPressed) {
@@ -103,43 +107,47 @@ int main() {
     if (Keyboard::isKeyPressed(Keyboard::Down)) delay = 0.05;
 
     //// <- Move -> ///
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < kBlockNumOfPiece; i++) {
       tmp_pos[i] = next_pos[i];
       next_pos[i].x += dx;
     }
-    if (!check())
-      for (int i = 0; i < 4; i++) next_pos[i] = tmp_pos[i];
+    if (!check()) next_pos = tmp_pos;
 
     //////Rotate//////
     if (rotate) {
       Point p = next_pos[1];  // center of rotation
-      for (int i = 0; i < 4; i++) {
+      for (int i = 0; i < kBlockNumOfPiece; i++) {
         int x = next_pos[i].y - p.y;
         int y = next_pos[i].x - p.x;
         next_pos[i].x = p.x - x;
         next_pos[i].y = p.y + y;
       }
-      if (!check())
-        for (int i = 0; i < 4; i++) next_pos[i] = tmp_pos[i];
+      if (!check()) next_pos = tmp_pos;
     }
 
     ///////Tick//////
     if (timer > delay) {
-      for (int i = 0; i < 4; i++) {
+      // 1 shift down
+      for (int i = 0; i < kBlockNumOfPiece; i++) {
         tmp_pos[i] = next_pos[i];
         next_pos[i].y += 1;
       }
 
+      // can't go down further
       if (!check()) {
-        for (int i = 0; i < 4; i++)
+        // pin the piece
+        for (int i = 0; i < kBlockNumOfPiece; i++) {
           field[tmp_pos[i].y][tmp_pos[i].x] = colorNum;
-
-        colorNum = 1 + rand() % 7;
-        int n = rand() % 7;
-        for (int i = 0; i < 4; i++) {
-          next_pos[i].x = figures[n][i] % 2;
-          next_pos[i].y = figures[n][i] / 2;
         }
+
+        colorNum = 1 + rand() % kFiguresNum;
+        // generate next rand piece
+        int n = rand() % kFiguresNum;
+        for (int i = 0; i < kBlockNumOfPiece; i++) {
+          next_pos[i].x = figures[n][i].x;
+          next_pos[i].y = figures[n][i].y;
+        }
+        // failed
         if (!check()) {
           window.close();
           break;
@@ -150,16 +158,16 @@ int main() {
     }
 
     ///////check lines//////////
-    int k = RowsNum - 1;
-    for (int i = RowsNum - 1; i > 0; i--) {
+    int k = kRowsNum - 1;
+    for (int i = kRowsNum - 1; i > 0; i--) {
       int count = 0;
-      for (int j = 0; j < ColumnsNum; j++) {
+      for (int j = 0; j < kColumnsNum; j++) {
         if (field[i][j]) {
           count++;
         }
         field[k][j] = field[i][j];
       }
-      if (count < ColumnsNum) {
+      if (count < kColumnsNum) {
         k--;
       } else {
         // increase user's score
@@ -167,7 +175,7 @@ int main() {
       }
     }
     if (k != 0) {
-      std::fill(field[0], field[0] + (ColumnsNum * (k + 1)), 0);
+      std::fill(field[0], field[0] + (kColumnsNum * (k + 1)), 0);
     }
 
     dx = 0;
@@ -178,20 +186,22 @@ int main() {
     window.clear(Color::White);
     window.draw(background);
 
-    for (int i = 0; i < RowsNum; i++)
-      for (int j = 0; j < ColumnsNum; j++) {
-        if (field[i][j] == 0) continue;
-        tiles_sprite.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
-        tiles_sprite.setPosition(j * 18, i * 18);
-        tiles_sprite.move(28, 31);  // offset
-        window.draw(tiles_sprite);
-      }
-
-    for (int i = 0; i < 4; i++) {
-      tiles_sprite.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
-      tiles_sprite.setPosition(next_pos[i].x * 18, next_pos[i].y * 18);
+    auto draw_spirit = [&](int color, int x, int y) {
+      tiles_sprite.setTextureRect(IntRect(color * 18, 0, 18, 18));
+      tiles_sprite.setPosition(x * 18, y * 18);
       tiles_sprite.move(28, 31);  // offset
       window.draw(tiles_sprite);
+    };
+
+    for (int i = 0; i < kRowsNum; i++) {
+      for (int j = 0; j < kColumnsNum; j++) {
+        if (field[i][j] == 0) continue;
+        draw_spirit(field[i][j], j, i);
+      }
+    }
+
+    for (int i = 0; i < kBlockNumOfPiece; i++) {
+      draw_spirit(colorNum, next_pos[i].x, next_pos[i].y);
     }
 
     window.draw(frame);
